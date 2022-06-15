fff7c3435d6d00e7497da566ce99700c77d9c417
timothyoesch <54284649+timothyoesch@users.noreply.github.com>
2022-06-16 00:53:08 +0200

    Frontpage block setup

--- wp-content/themes/to-mellon-mas/functions.php
+++ wp-content/themes/to-mellon-mas/functions.php
@@ -1,5 +1,43 @@
 <?php
 
+/* tmm */
+
+function tmm_scripts() {
+    wp_enqueue_style( 'fa', get_template_directory_uri() . "/lib/font-awesome/css/font-awesome.min.css", [], "1.0.0" );
+    wp_enqueue_style( 'theme', get_template_directory_uri() . '/dist/theme.css', [], "1.0.0" );
+    wp_enqueue_style( 'bundle', get_template_directory_uri() . '/dist/style.css', [], "1.0.0" );
+    wp_enqueue_script( 'bundle', get_template_directory_uri() . '/dist/app.js', array(), "1.0.0", true );
+}
+add_action( 'wp_enqueue_scripts', 'tmm_scripts' );
+
+function tmm_theme_support() {
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'post-thumbnails' );
+}
+add_action( 'after_setup_theme', 'tmm_theme_support' );
+
+function tmm_menus() {
+  register_nav_menu('tmm-main-nav',__( 'Main Navigation' ));
+  register_nav_menu('tmm-footer-nav',__( 'Footer Navigation' ));
+}
+add_action( 'init', 'tmm_menus' );
+
+function tmm_menu_items( $location, $args = [] ) {
+
+    // Get all locations
+    $locations = get_nav_menu_locations();
+
+    // Get object id by location
+    $object = wp_get_nav_menu_object( $locations[$location] );
+
+    // Get menu items by menu name
+    $menu_items = wp_get_nav_menu_items( $object->name, $args );
+
+    // Return menu post objects
+    return $menu_items;
+}
+
+
 // ACF
 
 function tmm_acf() {
@@ -36,5 +74,34 @@ function tmm_blocktypes() {
 
     // Check function exists.
     if( function_exists('acf_register_block_type') ) {
+        acf_register_block_type(array(
+            'name'              => 'heroine',
+            'title'             => __('Heroine Section'),
+            'description'       => __('Heroine Section for the homepage'),
+            'render_template'   => 'template-parts/blocks/heroine.php',
+            'category'          => 'tmm',
+            'icon'              => '',
+            'keywords'          => array("hero", "heroine", "section"),
+        ));
+
+        acf_register_block_type(array(
+            'name'              => 'intro',
+            'title'             => __('Intro'),
+            'description'       => __('Intro block for the homepage'),
+            'render_template'   => 'template-parts/blocks/intro.php',
+            'category'          => 'tmm',
+            'icon'              => '',
+            'keywords'          => array("intro", "section"),
+        ));
+
+        acf_register_block_type(array(
+            'name'              => 'shorts',
+            'title'             => __('Short Arguments'),
+            'description'       => __('Short Arguments block for the homepage'),
+            'render_template'   => 'template-parts/blocks/shorts.php',
+            'category'          => 'tmm',
+            'icon'              => '',
+            'keywords'          => array("Arguments", "toggles", "section"),
+        ));
     }
 }
---
+++ wp-content/themes/to-mellon-mas/template-parts/blocks/shorts.php
@@ -0,0 +1,3 @@
+<div style='height: 400px; background: #242424; display: flex; justify-content: center; align-items: center; border-radius: 5px'>
+    <p style='max-width: 50%; text-align: center; font-style: italic; color: #aaa'><b>This is a placeholder for the block 'Short arguments with toggles': The content will be displayed here, don't worry.</b></p>
+</div>
